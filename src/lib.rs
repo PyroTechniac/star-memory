193161a61bf7a0439ba63802378da9f1eac287ed
Gryffon Bellish <owenbellish@gmail.com>
2021-08-29 11:30:07 -0400

    rebrand

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,44 @@
 #![warn(clippy::pedantic, clippy::nursery, clippy::suspicious)]
 #![allow(clippy::missing_errors_doc)]
 
+use std::{
+    error::Error,
+    fmt::{Display, Formatter, Result as FmtResult},
+    time::Duration,
+};
+
+#[derive(Debug)]
+pub struct VoidError;
+
+impl Display for VoidError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        f.write_str("this shouldn't happen")
+    }
+}
+
+impl Error for VoidError {}
+
 #[cfg(unix)]
 mod unix;
 #[cfg(windows)]
 mod win;
 
-#[cfg(any(unix, doc))]
-pub fn get() -> procfs::ProcResult<u64> {
-    unix::get()
+#[cfg(unix)]
+pub fn memory() -> procfs::ProcResult<u64> {
+    unix::get_memory()
 }
 
-#[cfg(any(windows, doc))]
-pub fn get() -> windows::Result<u64> {
-    win::get()
+#[cfg(unix)]
+pub fn uptime() -> Result<Duration, unix::UnixError> {
+    unix::get_uptime()
+}
+
+#[cfg(windows)]
+pub fn memory() -> windows::Result<u64> {
+    win::get_memory()
+}
+
+#[cfg(windows)]
+pub fn uptime() -> Result<Duration, VoidError> {
+    win::get_uptime()
 }
--- src/win.rs
+++ src/win.rs
@@ -1,8 +1,15 @@
+use super::VoidError;
+use std::time::Duration;
 use win_bindings::Windows::System::Diagnostics::ProcessDiagnosticInfo;
 
-pub fn get() -> windows::Result<u64> {
+pub fn get_memory() -> windows::Result<u64> {
     ProcessDiagnosticInfo::GetForCurrentProcess()?
         .MemoryUsage()?
         .GetReport()?
         .PageFileSizeInBytes()
+}
+
+pub fn get_uptime() -> Result<Duration, VoidError> {
+    let ret = unsafe { kernel32::GetTickCount64() };
+    Ok(Duration::from_millis(ret))
 }
