4a1be7b15e0d8e73999545ce3b87b6b5c7fc0585
Gryffon Bellish <owenbellish@gmail.com>
2021-08-31 23:04:21 +0000

    rewrite unix for a third time ffs

--- src/unix.rs
+++ src/unix.rs
@@ -1,29 +1,92 @@
+use procfs::process::Stat;
 use std::{
     error::Error,
     fmt::{Display, Formatter, Result as FmtResult},
+    fs::read,
+    io::{Cursor, Error as IoError},
+    path::{Path, PathBuf},
 };
-use sysinfo::{get_current_pid, ProcessExt, RefreshKind, System, SystemExt};
 
 #[derive(Debug)]
-pub struct UnixError;
+pub struct UnixError {
+    kind: UnixErrorType,
+    source: Option<Box<dyn Error + Send + Sync>>,
+}
 
-impl Display for UnixError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        f.write_str("an error occurred getting memory usage")
+impl UnixError {
+    pub const fn kind(&self) -> &UnixErrorType {
+        &self.kind
+    }
+
+    pub fn into_source(self) -> Option<Box<dyn Error + Send + Sync>> {
+        self.source
+    }
+
+    pub fn into_parts(self) -> (UnixErrorType, Option<Box<dyn Error + Send + Sync>>) {
+        (self.kind, self.source)
+    }
+
+    fn system_call(source: IoError) -> Self {
+        Self {
+            kind: UnixErrorType::SystemCall,
+            source: Some(Box::new(source)),
+        }
+    }
+
+    fn file_read(source: IoError, path: PathBuf) -> Self {
+        Self {
+            kind: UnixErrorType::FileRead(path),
+            source: Some(Box::new(source)),
+        }
+    }
+
+    fn file_contents_malformed() -> Self {
+        Self {
+            kind: UnixErrorType::FileContentsMalformed,
+            source: None,
+        }
     }
 }
 
-impl Error for UnixError {}
+impl Display for UnixError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self.kind {
+            UnixErrorType::FileRead(ref path) => {
+                f.write_str("failed to read from file ")?;
+                Display::fmt(&path.display(), f)
+            }
+            UnixErrorType::FileContentsMalformed => {
+                f.write_str("file contents are in unexpected format")
+            }
+            UnixErrorType::SystemCall => f.write_str("call to system-native API errored"),
+        }
+    }
+}
 
-#[cfg(unix)]
-#[allow(clippy::cast_sign_loss)]
+impl Error for UnixError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        self.source
+            .as_ref()
+            .map(|source| &**source as &(dyn Error + 'static))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum UnixErrorType {
+    FileRead(PathBuf),
+    FileContentsMalformed,
+    SystemCall,
+}
+
 pub fn get_memory() -> Result<u64, UnixError> {
-    let refresh_kind = RefreshKind::new().with_memory();
-    let system = System::new_with_specifics(refresh_kind);
+    let bytes_per_page = procfs::page_size().map_err(UnixError::system_call)?;
 
-    let current_process = system
-        .process(get_current_pid().map_err(|_| UnixError)?)
-        .ok_or(UnixError)?;
+    let path = Path::new("/proc/self/stat");
+    let file_contents = read(path).map_err(|err| UnixError::file_read(err, path.to_path_buf()))?;
 
-    Ok(current_process.memory() * 1024)
+    let readable_string = Cursor::new(file_contents);
+    let stat_file =
+        Stat::from_reader(readable_string).map_err(|_| UnixError::file_contents_malformed())?;
+
+    Ok((stat_file.rss as u64) * (bytes_per_page as u64))
 }
